2ca18c6cab198142d0a2d33aa60e8d8c02615c67
Alice He <alicehe4@gmail.com>
2019-01-17 15:01:24 -0500

    Finished Sequelize Essentials

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const app = express()
 const morgan = require('morgan')
 const html = require('html-template-tag')
 const layout = require('./views/layout')
-
-// app.use('/', require('./views/index'))
+const {db, Page, User} = require('./models')
 
 app.use(express.static(__dirname + '/public'))
 
 app.use(express.urlencoded({extended: false})) // false means we do not accept nested objects
 
 app.get('/', (req, res) => {
-    // res.send('hello')
     res.send(layout(''))
 })
 
-app.listen(3000, () => {
-    console.log('Listening')
+const init = async () => {
+    await db.sync({force:true})
+
+    app.listen(3000, () => {
+        console.log('Listening')
+    })
+}
+
+init()
+
+db.authenticate().then(() => {
+    console.log(db)
 })
---
+++ models/index.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack'
+    //, {loggig: false}     would remove the sequel command from terminal when sync is run
+    );
+
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    slug: {
+        type: Sequelize.STRING,//CHAR LIMIT OF 255
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT, //CHAR LIMIT OF 30K
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open','closed'), //ALLOWED VALUES
+    }
+
+})
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
+});
+
+module.exports = {db, Page, User}
